288214bb4f08eded6c09aa4704d5e46ed9c11338
15077693d <15077693d@gmail.com>
2020-07-04 16:59:55 +0800

    implement parse method for name and gdp_debt

--- projects/national_debt/national_debt/spiders/gdp_debt.py
+++ projects/national_debt/national_debt/spiders/gdp_debt.py
@@ -7,4 +7,11 @@ class GdpDebtSpider(scrapy.Spider):
     start_urls = ['http://worldpopulationreview.com/countries/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        rows = response.xpath('(//div[@class="col-md-12"])[1]//tr')
+        for row in rows:
+            country_name = row.xpath('.//td[1]//text()').get()
+            gdp_debt = row.xpath('.//td[2]//text()').get()
+            yield {
+                "country_name":country_name,
+            "gdp_debt":gdp_debt,
+            }
