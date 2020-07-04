3c536278091022434e28ed9f967400574600d329
15077693d <15077693d@gmail.com>
2020-07-04 16:47:35 +0800

    create 'gdp_debt' spider

---
+++ projects/national_debt/national_debt/spiders/gdp_debt.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class GdpDebtSpider(scrapy.Spider):
+    name = 'gdp_debt'
+    allowed_domains = ['worldpopulationreview.com']
+    start_urls = ['http://worldpopulationreview.com/countries/countries-by-national-debt/']
+
+    def parse(self, response):
+        pass
